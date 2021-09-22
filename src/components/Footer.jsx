795c71084b74e3ed493f5e252b29374322825c30
Ilya <ilyaprikaz@gmail.com>
2021-09-22 23:15:11 +0400

    feat: create header and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  const fullName = "Ilya Prikazchikov";
+  return (
+    <footer>
+      <p>
+        {fullName} ⓒ {year}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <img
+          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/clapper-board_1f3ac.png"
+          draggable="false"
+        />{" "}
+        MovieMate
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
