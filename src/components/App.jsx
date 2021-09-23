9ca259704b01d22113dfb5efeab79ef442a0d4ad
Ilya <ilyaprikaz@gmail.com>
2021-09-23 19:37:53 +0400

    feat: create title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Title from "./Title";
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Title />
       <Footer />
     </div>
   );
---
+++ src/components/Title.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div id='title'>
+    <h3>Cover</h3>
+    <h3>Name</h3>
+    <h3>Genre</h3>
+    <h3>Country</h3>
+    <h3>Runtime</h3>
+    <h3>Rating</h3>
+    </div>
+  );
+}
+
+export default Title;
