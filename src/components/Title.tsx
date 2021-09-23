d501830d2cca2188a9fb152b5c8547dfec181e39
Ilya <ilyaprikaz@gmail.com>
2021-09-23 21:07:28 +0400

    feat: make css more adaptive

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 function Title() {
   return (
-    <div id='title'>
-    <h3>Cover</h3>
-    <h3>Name</h3>
-    <h3>Genre</h3>
-    <h3>Country</h3>
-    <h3>Runtime</h3>
-    <h3>Rating</h3>
+    <div id="title">
+      <h3>Cover</h3>
+      <h3>Name</h3>
+      <h3>Genre</h3>
+      <h3>Country</h3>
+      <h3>Runtime</h3>
+      <h3>Rating</h3>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import Test from "./components/App";
 
-
 class App extends React.Component {
-    render() {
-        return <Test />
-    }
+  render() {
+    return <Test />;
+  }
 }
 
 render(<App />, document.getElementById("root"));
