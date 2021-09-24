98499fe35618c175e01f44809801d2950e254f94
Ilya <ilyaprikaz@gmail.com>
2021-09-24 20:05:25 +0400

    feat: create start pagination

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,29 +13,54 @@ function App() {
   const [showsData, setShows] = useState<ShowI[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [showsPerPage] = useState<number>(20);
+  const [showsPerPage] = useState<number>(50);
   const [ApiPage, setApiPage] = useState<number>(0);
+  const [addition, setAddition] = useState<number>(0);
 
+  /** GET SHOWS DATA */
   useEffect(() => {
-    const fetchShows = async () => {
+    const fetchShows = async (page: number) => {
       setLoading(true);
       const res: AxiosResponse<ShowI[]> = await axios.get<ShowI[]>(
-        "https://api.tvmaze.com/shows?page=" + ApiPage
+        "https://api.tvmaze.com/shows?page=" + page
       );
       setShows(res.data);
       setLoading(false);
     };
 
-    fetchShows();
+    fetchShows(ApiPage);
   }, []);
+
+  /** Получить ApiPage по номеру страницы */
+  function getApiPageByPickedPage(pickedPage: number) {
+    let apiPage = Math.floor(((pickedPage - 1) * 50) / 250);
+    return apiPage;
+  }
 
   // Get current shows
   const indexOfLastShow = currentPage * showsPerPage;
   const indexOfFirstShow = indexOfLastShow - showsPerPage;
   const currentShows = showsData.slice(indexOfFirstShow, indexOfLastShow);
 
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  /** Change page */
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber); // Переключаем страницу
+
+    // страница API
+    let oldApiPage = ApiPage; // Старая страница API
+    let currentApiPage = getApiPageByPickedPage(pageNumber); // Новая страница API
+
+      // Если они не равны, то хукаем
+    if (oldApiPage !== currentApiPage) {
+      if (currentApiPage < oldApiPage) {
+        setAddition(addition - 5);
+      }
+      if (oldApiPage < currentApiPage) {
+        setAddition(addition + 5);
+      }
+      setApiPage(currentApiPage);
+    }
+  };
 
   return (
     <div>
@@ -46,6 +71,7 @@ function App() {
         showsPerPage={showsPerPage}
         totalShows={showsData.length}
         paginate={paginate}
+        addition={addition}
       />
       <Footer loading={loading} />
     </div>
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -3,16 +3,26 @@ const Pagination = (props: {
   showsPerPage: number;
   totalShows: number;
   paginate: Function;
+  addition: number
 }) => {
   const pageNumbers: number[] = [];
 
   for (let i = 1; i <= Math.ceil(props.totalShows / props.showsPerPage); i++) {
-    pageNumbers.push(i);
+    pageNumbers.push(i + props.addition);
   }
 
   return (
-    <nav className="pag-bar">
-      <ul className="pagination ">
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.length !== 0 && pageNumbers[0] !== 1 && <li key={pageNumbers[0] - 1} className="page-item">
+          <a
+            onClick={() => props.paginate(pageNumbers[0] - 1)}
+            href="#"
+            className="page-link"
+          >
+            {pageNumbers[0] - 1}
+          </a>
+        </li>}
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
             <a
@@ -24,6 +34,15 @@ const Pagination = (props: {
             </a>
           </li>
         ))}
+        <li key={pageNumbers[pageNumbers.length - 1] + 1} className="page-item">
+          <a
+            onClick={() => props.paginate(pageNumbers[pageNumbers.length - 1] + 1)}
+            href="#"
+            className="page-link"
+          >
+            {pageNumbers[pageNumbers.length - 1] + 1}
+          </a>
+        </li>
       </ul>
     </nav>
   );
